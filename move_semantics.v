a455a46db84be73cea46236d30199605eb5aa348
Roland Ruiters-Christou <roland.ruiters@autodesk.com>
2022-02-10 00:45:26 +0100

    WIP

--- move_semantics.v
+++ move_semantics.v
@@ -7,6 +7,7 @@ From Coq Require Import Lists.List.
 From Coq Require Import List. Import ListNotations.
 Require Import Unicode.Utf8.
 Require Import Coq.Program.Wf.
+Require Import Omega.
 
 Inductive id : Type := 
   | Id  (n : nat).
@@ -203,6 +204,199 @@ Definition SplitHelper (f1 : nat -> side -> Operation)  (f2 : nat -> side-> Oper
                 | right, left => ( ((f1 x s), ([(f2 x right)])))
                 | _, _ => (pair (f1 x s) ([]))
               end)).
+
+Definition getSplitOpLength (iterDef : IterationDefinition) (o : Operation) :=
+  let (x, y) := (iterDef.(splitOperation) o 2 left) in
+  (length y).
+
+(* Definition testDef (n : nat) : (nat * nat) := (3, 1).
+
+Lemma testDefFstCond: forall n:nat, (fst (testDef n)) > 0.
+Proof.
+intros.
+unfold testDef.
+unfold fst.
+auto.
+Qed.
+
+Lemma testDefSndCond: forall n:nat, (snd (testDef n)) > 0.
+Proof.
+intros.
+unfold testDef.
+unfold snd.
+auto.
+Qed.
+
+Definition testDef2 (n : nat) : nat := 
+  let '(x,y) := (testDef n) in (x + y).
+
+Lemma testDef2SumCond: forall n:nat, (testDef2 n) > 0.
+Proof.
+intros.
+unfold testDef2.
+pose testDefFstCond.
+pose testDefSndCond.
+set (A := (testDef n)).
+rewrite surjective_pairing with (p:=A).
+set (A1 := fst A).
+set (A2 := snd A).
+specialize g with (n:=n) as V.
+fold A in V.
+fold A1 in V.
+specialize g0 with (n:=n) as V2.
+fold A in V2.
+fold A2 in V2.
+
+(*pose (forall (A : Type)  (T : (A * A)), T = ( (fst T), (fst T))).*)
+(* assert (forall (X : Type) (Y : Type)  (T : (X * Y)), T = ( (fst T), (snd T))).
+intros.
+unfold fst.
+unfold snd.
+destruct T.
+trivial.*)
+rewrite > H with (T:=A).
+subst A.
+
+
+(* apply V in A. *)
+set (A := ((fst A), (fst A))).
+
+
+
+
+Definition my_call {A B C} (f : A -> B * C) (a : A) : C :=
+  let (b, c) := f a in c.
+
+Lemma mycall_is_call {A B C} (f : A -> B * C) a :
+  my_call f a = snd (f a).
+Proof.
+  unfold my_call.
+  set (bx := (snd (f a))).
+  (* unfold snd in bx. *)
+  fold bx.
+  trivial.
+Qed. *)
+
+
+(* Lemma splitOpLengthLength: forall (iterDef : IterationDefinition) (o:Operation), ((getSplitOpLength iterDef o) <= 1).
+Proof.
+intro.
+intro.
+unfold getSplitOpLength.
+(* set (split := (snd (splitOperation iterDef o 2 left))).*)
+destruct (splitOperation iterDef o 2 left) as (a, b).
+
+
+(* destruct (splitOperation iterDef o 2 left) as (x, y). *)
+apply splitOperationSequenceLength_cond.
+Qed.*)
+
+Lemma concat_length: forall (o : Operation) (t : list Operation), length(o::t) = (length(t)) + 1.
+Proof.
+intros.
+unfold length.
+omega.
+Qed.
+
+
+Program Fixpoint iterateOverOperationLists (iterDef : IterationDefinition) (ol1 : list Operation) (ol2 : list Operation) 
+  {measure ((length ol1) + (length ol2)) } : (list Operation) :=
+  match ol1 with
+    | o1::t1 => match ol2 with
+      | o2::t2 => 
+        let SAInfo := iterDef.(getLengthInSequenceA) o1 in
+        let SBInfo := iterDef.(getLengthInSequenceB) o2 in
+
+        let len1 := (fst SAInfo) in
+        let len2 := (fst SBInfo) in
+        let s1 := (snd SAInfo) in
+        let s2 := (snd SBInfo) in
+
+        let splitOp     := if len2 <? len1 then o1 else o2 in
+        let splitLength := if len2 <? len1 then len2 else len1 in
+        let splitSide   := if len2 <? len1 then s2 else s1 in
+
+        let splitOp := (iterDef.(splitOperation) splitOp splitLength splitSide) in
+        (* let (truncatedOp, remSeq) := splitOp in *)
+        let opA :=  if len2 <? len1 then (fst splitOp) else o1 in
+        let opB :=  if len2 <? len1 then o2 else (fst splitOp) in
+        let seqA := if len2 <? len1 then (snd splitOp) ++ t1 else t1 in
+        let seqB := if len2 <? len1 then t2 else (snd splitOp) ++ t2 in
+
+        (*let '(opA, opB, seqA, seqB) :=  (if len2 <? len1 then (
+            (truncatedOp, o2, remSeq ++ t1, t2)
+          )
+          else (
+            (o1, truncatedOp, t1, remSeq ++ t2)
+          )) in*)
+
+(*        let truncatedO2 := if len1 <? len2 then o2 else o2 in *)
+        (iterateOverOperationLists iterDef seqA seqB)
+      | nil => ol1
+      end
+    | nil => ol2
+  end.
+Next Obligation.
+ (* intros.
+  Set Printing All.*)
+
+case_eq (fst (getLengthInSequenceB iterDef o2) <? fst (getLengthInSequenceA iterDef o1)).
+- intros. 
+  rewrite app_length.
+  set (X := Datatypes.length (snd (splitOperation iterDef o1 (fst (getLengthInSequenceB iterDef o2)) (snd (getLengthInSequenceB iterDef o2))))).
+  pose splitOperationSequenceLength_cond.
+  specialize l with (i := iterDef) (o :=o1) (x := (fst (getLengthInSequenceB iterDef o2))) (s := (snd (getLengthInSequenceB iterDef o2))).
+  fold X in l.
+  rewrite concat_length.
+  rewrite concat_length.
+  omega.
+- intros. 
+  rewrite app_length.
+  set (X := (Datatypes.length (snd (splitOperation iterDef o2 (fst (getLengthInSequenceA iterDef o1)) (snd (getLengthInSequenceA iterDef o1)))))).
+  pose splitOperationSequenceLength_cond.
+  specialize l with (i := iterDef) (o :=o2) (x := (fst (getLengthInSequenceB iterDef o1))) (s := (snd (getLengthInSequenceB iterDef o1))).
+  fold X in l.
+  rewrite concat_length.
+  rewrite concat_length.
+  omega.
+
+
+  Set Printing All.
+
+  (* fold SO2 in SO.
+  rewrite surjective_pairing with (p:=SO2) in SO. *)
+(* Program Fixpoint iterateOverOperationLists (iterDef : IterationDefinition) (ol1 : list Operation) (ol2 : list Operation) 
+  {measure ((length ol1) + (length ol2)) } : (list Operation) :=
+  match ol1 with
+    | o1::t1 => match ol2 with
+      | o2::t2 => 
+        let '(len1, s1) := iterDef.(getLengthInSequenceA) o1 in
+        let '(len2, s2) := iterDef.(getLengthInSequenceB) o2 in
+        
+        let '(opA, opB, seqA, seqB) :=  (if len2 <? len1 then (
+            let (truncatedOp, remSeq) := (iterDef.(splitOperation) o1 len2 s2) in
+            (truncatedOp, o2, remSeq ++ t1, t2)
+          )
+          else (
+            let (truncatedOp, remSeq) := (iterDef.(splitOperation) o2 len1 s1) in
+            (o1, truncatedOp, t1, remSeq ++ t2)
+          )) in
+
+(*        let truncatedO2 := if len1 <? len2 then o2 else o2 in *)
+        [o1] ++ (iterateOverOperationLists iterDef seqA seqB)
+      | nil => ol1
+      end
+    | nil => ol2
+  end.
+Next Obligation.
+case_eq (len2 <? len1).
+- intros. 
+  replace (len2 <? len1) with true in Heq_anonymous.
+  set (SO := (let (truncatedOp, remSeq) := splitOperation iterDef o1 len2 s2 in (truncatedOp, o2, remSeq ++ t1, t2))).
+  fold SO in Heq_anonymous.
+  set (SO2 := splitOperation iterDef o1 len2 s2).
+  Set Printing All.*)
+
 Definition SquashIterationDefinition :=  
   {| 
      getLengthInSequenceA := fun (op : Operation) => match op with 
@@ -246,31 +440,6 @@ Definition SquashIterationDefinition :=
 Eval compute in SquashIterationDefinition.(splitOperation) (Skip> 9) 8 right.
 Eval compute in SquashIterationDefinition.(splitOperation) (Remove< [<$0, 0>; <$1, 1>; <$2, 2>]) 0 right.
 
-
-Program Fixpoint iterateOverOperationLists (iterDef : IterationDefinition) (ol1 : list Operation) (ol2 : list Operation) 
-  {measure ((length ol1) + (length ol2)) } : (list Operation) :=
-  match ol1 with
-    | o1::t1 => match ol2 with
-      | o2::t2 => 
-        let '(len1, s1) := iterDef.(getLengthInSequenceA) o1 in
-        let '(len2, s2) := iterDef.(getLengthInSequenceB) o2 in
-        
-        let '(opA, opB, seqA, seqB) := (if len2 <? len1 then (
-            let '(truncatedOp, remSeq) := (iterDef.(splitOperation) o1 len2 s2) in
-            (truncatedOp, o2, remSeq ++ t1, t2)
-          )
-          else (
-            let '(truncatedOp, remSeq) := (iterDef.(splitOperation) o2 len1 s1) in
-            (o1, truncatedOp, t1, remSeq ++ t2)
-          )) in 
-
-(*        let truncatedO2 := if len1 <? len2 then o2 else o2 in *)
-        [o1] ++ (iterateOverOperationLists iterDef seqA seqB)
-      | nil => ol1
-      end
-    | nil => ol2
-  end.
-Next Obligation.
 
 
 (*Definition same_id (a : id) (b : id) : bool :=
